2ad67787a1de8c79df34b96731efa1d456272321
Afnan-Mohamed <afnan.am95@gmail.com>
2021-11-10 20:59:45 +0200

    Please Read The Power Point First

---
+++ library/admin/includes/footer.php
@@ -0,0 +1,10 @@
+<section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2020 Online Library Management System @ Yat Center @ Afnan Mohamed</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
